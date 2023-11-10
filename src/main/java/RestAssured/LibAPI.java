17b89f6cd0cc2b23196416b5df3c8e8d57be15e3
abobakr982022 <abobakrahmed982022@gmail.com>
2023-11-10 21:33:30 +0200

    handle with static File json payloads

--- src/main/java/RestAssured/LibAPI.java
+++ src/main/java/RestAssured/LibAPI.java
@@ -8,6 +8,12 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class LibAPI {
     @Test(dataProvider = "Data")
     public void AddBook(String isbn , String aisle){
@@ -17,7 +23,18 @@ public class LibAPI {
         System.out.println("res " + ResLib);
         String Msg = RawToJSON.RawToJson(ResLib).getString("Msg");
         System.out.println("Msg: " + Msg);
-        Assert.assertEquals(Msg,"successfully added");
+        //.assertEquals(Msg,"successfully added");
+        System.out.println("");
+    }
+    @Test
+    public void AddBook2() throws IOException {
+        baseURI="http://216.10.245.166";
+        String ResLib = given().log().all().body(new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir")+"/src/main/java/new 3.json")))).when().post("/Library/Addbook.php")
+                .then().log().all().assertThat().statusCode(200).extract().response().asString();
+        System.out.println("res " + ResLib);
+        String Msg = RawToJSON.RawToJson(ResLib).getString("Msg");
+        System.out.println("Msg: " + Msg);
+        //Assert.assertEquals(Msg,"successfully added");
         System.out.println("");
     }
 
